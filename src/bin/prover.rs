15d44bf23ab76a5006cb220518c0d0b0ebb9a9dc
Anhelina Kruk <krukanhelina06@gmail.com>
2025-05-23 12:31:39 +0200

    add prints

--- src/bin/prover.rs
+++ src/bin/prover.rs
@@ -46,6 +46,7 @@ async fn prover<T: AsyncRead + AsyncWrite + Unpin + Send + 'static>(
     uri: &str,
 ) {
     let uri = uri.parse::<Uri>().unwrap();
+    println!("URI: {:?}", uri);
     assert_eq!(uri.scheme().unwrap().as_str(), "https");
 
     let server_domain = uri.authority().unwrap().host();
@@ -82,24 +83,30 @@ async fn prover<T: AsyncRead + AsyncWrite + Unpin + Send + 'static>(
 
     // Connect to TLS Server.
     let tls_client_socket = TcpStream::connect(server_addr).await.unwrap();
+    println!("Connected to TLS Server");
 
     // Pass server connection into the prover.
     let (mpc_tls_connection, prover_fut) = prover.connect(tls_client_socket).await.unwrap();
+    println!("Connected to Verifier");
 
     // Wrap the connection in a TokioIo compatibility layer to use it with hyper.
     let mpc_tls_connection = FuturesIo::new(mpc_tls_connection);
+    println!("Wrapped connection in FuturesIo compatibility layer");
 
     // Spawn the Prover to run in the background.
     let prover_task = smol::spawn(prover_fut);
+    println!("Spawned the Prover to run in the background");
 
     // MPC-TLS Handshake.
     let (mut request_sender, connection) =
         hyper::client::conn::http1::handshake(mpc_tls_connection)
             .await
             .unwrap();
+    println!("Handshake completed");
 
     // Spawn the connection to run in the background.
     let _ = smol::spawn(connection);
+    println!("Spawned the connection to run in the background");
 
     let request = Request::builder()
         .uri(uri.clone())
@@ -109,17 +116,24 @@ async fn prover<T: AsyncRead + AsyncWrite + Unpin + Send + 'static>(
         .method("GET")
         .body(Empty::<Bytes>::new())
         .unwrap();
+    println!("Request {:?}", request);
     let response = request_sender.send_request(request).await.unwrap();
+    println!("Request sent");
 
     assert!(response.status() == StatusCode::OK);
+    println!("Response received");
 
     let mut prover = prover_task.await.unwrap().start_prove();
+    println!("Started the Prover");
     // Reveal parts of the transcript.
     let idx_sent = revealed_ranges_sent(&mut prover);
+    println!("Revealed ranges for sent data");
     let idx_recv = revealed_ranges_received(&mut prover);
+    println!("Revealed ranges for received data");
     prover.prove_transcript(idx_sent, idx_recv).await.unwrap();
-
+    println!("Proved the transcript");
     prover.finalize().await.unwrap();
+    println!("Finalized the prover");
 }
 
 /// Returns the received ranges to be revealed to the verifier.
